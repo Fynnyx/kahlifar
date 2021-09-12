7a47f609702488038009eb2c8039f1a636b1c625
Fynnyx <westerathf@gmail.com>
2021-09-12 22:23:18 +0200

    added social media and beta of clear

--- bot/bot.py
+++ bot/bot.py
@@ -2,9 +2,10 @@ from asyncio.protocols import DatagramProtocol
 import discord
 from discord import embeds
 from discord import channel
+from discord import message
 
 from discord.ext import commands
-from discord.ext.commands import has_permissions, CheckFailure
+from discord.ext.commands import has_permissions, MissingPermissions, CheckFailure
 
 # from discord_slash import SlashCommand, SlashContext
 
@@ -38,7 +39,7 @@ async def status_task():
             await client.change_presence(activity=discord.Game(messages[x]), status=discord.Status.online)
             await asyncio.sleep(time)
 
-            
+
 
 @client.event
 async def on_ready():
@@ -49,6 +50,11 @@ async def on_ready():
 # async def on_member_join(member):
 #     print("New Member")
 #     await client.send_message(member,"Welcome!")
+
+
+@client.event
+async def send_error(ctx, error, channel):
+    await ctx,channel.send("⛔Error: %s" % error)
 
 
 
@@ -65,7 +71,33 @@ async def discord_link(ctx):
 async def server_ip(ctx):
     await ctx.channel.send(data["properties"]["server_ip"])
 
+@client.command(aliases=list(data["properties"]["commands"]["social_media"]["aliases"]))
+async def social_media(ctx):
+    await ctx.channel.send("Social Media Links comming soon")
 
+
+@client.command()
+@has_permissions(manage_messages=True)
+async def clear(ctx):
+    args = message.content.split(' ')
+    print(args)
+    if len(args) == 2:
+        amount = args[2]
+        await ctx.channel.purge(limit=amount)
+        await ctx.channel.send("Deleted %s messaged" % amount) 
+    elif len(args) == 1:
+        amount = 1
+        await ctx.channel.purge(limit=amount)
+        await ctx.channel.send("Deleted %s messaged" % amount) 
+    else:
+        await send_error("Please try this format -> ´-clear [amount]´")
+        return
+
+@clear.error
+async def clear_error(ctx, error):
+    if isinstance(error, MissingPermissions):
+        text = "Sorry {}, you do not have permissions to do that!".format(ctx.message.author)
+        await ctx.send_message(ctx.message.channel, text)
 
 
 client.run(TOKEN)
