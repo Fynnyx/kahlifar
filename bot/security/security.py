c0ddbe20fb7b4f652974a24172ce3a6d816f2a27
Fynnyx <westerathf@gmail.com>
2021-10-07 19:24:17 +0200

    added syncronization for nickname, bug in role syncing

--- bot/security/security.py
+++ bot/security/security.py
@@ -1,7 +1,6 @@
 from os import name
 import discord
 from discord import Member
-from discord import channel
 from discord.ext import commands
 import discord.utils
 import asyncio
@@ -14,12 +13,24 @@ TOKEN = data["properties"]["token"]
 PREFIX = data["properties"]["prefix"]
 
 intents = discord.Intents()
-intents.guilds = True
-intents.members = True
-intents.emojis = True
-intents.messages = True
-intents.reactions = True
+intents.bans = False
 intents.dm_messages = True
+intents.dm_reactions= False
+intents.dm_typing= False
+intents.emojis = True
+intents.guild_messages = False
+intents.guild_reactions = False
+intents.guild_typing = False
+intents.guilds = True
+intents.integrations = False
+intents.invites = True
+intents.members = True
+intents.messages = True
+intents.presences= False
+intents.reactions = True
+intents.typing= False
+intents.voice_states= False
+intents.webhooks= False
 
 client = commands.Bot(command_prefix=PREFIX, help_command=None, intents=intents)
 
@@ -71,6 +82,7 @@ async def sync_member(member):
             if discord.utils.get(member.guild.roles, name=role.name) and str(role.name) != "@everyone":
                 game_role = discord.utils.get(member.guild.roles, name=role.name)
                 await member.add_roles(game_role)
+        await sync_nick(member.id, gen_member.display_name)
     else:
         await member.send(data["properties"]["general"]["events"]["sync_member"]["nv_message"] % (data["properties"]["general"]["infinite_invite"]))
 
@@ -78,8 +90,6 @@ async def sync_member(member):
 async def is_verified(game_member, gen_member):
     for role in gen_member.roles:
         if role.id == data["properties"]["general"]["events"]["on_reaction_add"]["verify"]["role"]:
-            # print(role.id)
-            # print(data["properties"]["general"]["events"]["on_reaction_add"]["verify"]["role"])
             return True
 
 
@@ -89,7 +99,49 @@ async def verify_user(user):
     role = discord.utils.get(user.guild.roles, id=data["properties"][server]["events"]["on_reaction_add"]["verify"]["role"])
     await user.add_roles(role)
 
-# Events    ----------------------------------------------------------------------------
+async def sync_nick(member_id, nick):
+    gen_guild = discord.utils.get(client.guilds, id=data["properties"]["general"]["guild_id"])
+    game_guild = discord.utils.get(client.guilds, id=data["properties"]["gaming"]["guild_id"])
+    gen_user = discord.utils.get(gen_guild.members, id=member_id)
+    game_user = discord.utils.get(game_guild.members, id=member_id)
+    try:
+        await gen_user.edit(nick=nick)
+        await game_user.edit(nick=nick)
+    except discord.errors.Forbidden:
+        return
+
+async def sync_user_roles(member_id, guild):
+    print(member_id, guild)
+    if guild.id == data["properties"]["general"]["guild_id"]:
+        gen_guild = guild
+        game_guild = discord.utils.get(client.guilds, id=(data["properties"]["gaming"]["guild_id"]))
+        game_user = discord.utils.get(game_guild.members, id=member_id)
+        gen_user = discord.utils.get(gen_guild.members, id=member_id)
+        for role in game_user.roles:
+            if str(role.name) != "@everyone":
+                await game_user.remove_roles(role)
+        for role in gen_user.roles:
+            if discord.utils.get(game_guild.roles, name=role.name) and str(role.name) != "@everyone":
+                game_role = discord.utils.get(game_guild.roles, name=role.name)
+                await game_user.add_roles(game_role)            
+        
+    if guild.id == data["properties"]["gaming"]["guild_id"]:
+        gen_guild = discord.utils.get(client.guilds, id=id(data["properties"]["general"]["guild_id"]))
+        game_guild = guild
+        gen_user = discord.utils.get(gen_guild.members, id=member_id)
+        game_user = discord.utils.get(game_guild.members, id=member_id)
+        for role in gen_user.roles:
+            if str(role.name) != "@everyone":
+                await gen_user.remove_roles(role)
+        for role in game_user.roles:
+            if discord.utils.get(gen_guild.roles, name=role.name) and str(role.name) != "@everyone":
+                gen_role = discord.utils.get(gen_guild.roles, name=role.name)
+                await gen_user.add_roles(gen_role)
+
+
+
+
+# Listerners    ----------------------------------------------------------------------------
 
 @client.event
 async def on_member_join(member):
@@ -106,16 +158,35 @@ async def on_member_join(member):
 
 @client.event
 async def on_reaction_add(reaction, user):
-    # print(reaction)
+    # if user
+    print(reaction)
     if reaction.message.guild.id == data["properties"]["general"]["guild_id"]:
         server = "general"
     elif reaction.message.guild.id == data["properties"]["gaming"]["guild_id"]:
         server = "gaming"
     # VERIFY
     if reaction.emoji == data["properties"][server]["events"]["on_reaction_add"]["verify"]["emoji"] and not user.bot:
-        print("verify")
         await verify_user(user)
+    # SELF ROLES
+    for self_role in data["properties"]["gaming"]["events"]["on_reaction_add"]["self_roles"]:
+        # print(self_role)
+        # print(data["properties"]["gaming"]["events"]["on_reaction_add"]["self_roles"][str(self_role)]["role"])
+        if reaction.emoji == str(self_role):
+            gen_guild = discord.utils.get(client.guilds, id=data["properties"]["general"]["guild_id"])
+            role = discord.utils.get(gen_guild.roles, id=data["properties"]["gaming"]["events"]["on_reaction_add"]["self_roles"][str(self_role)]["role"])
+            await user.add_roles(role)
+            print(role)
 
+@client.event
+async def on_member_update(before, after):
+    if before.roles != after.roles:
+        print(before.roles)
+        print(after.roles)
+        await sync_user_roles(after.id, after.guild)
+    if before.nick != after.nick:
+        # print(before.nick)
+        # print(after.nick)
+        await sync_nick(after.id, after.nick)
 
 # On Ready  ----------------------------------------------------------------------------
 
@@ -126,21 +197,21 @@ async def on_ready():
     await send_verify()
 
 async def send_verify():
-    for guild in client.guilds:
-        if int(guild.id) == data["properties"]["general"]["guild_id"]:
-            server = "general"
-        # elif int(guild.id) == data["properties"]["gaming"]["guild_id"]:
-        #     server = "gaming"
-            verify_channel = discord.utils.get(guild.channels, id=data["properties"][server]["events"]["on_reaction_add"]["verify"]["channel"])
-            verify_emoji = data["properties"][server]["events"]["on_reaction_add"]["verify"]["emoji"]
-            verify_message = data["properties"][server]["events"]["on_reaction_add"]["verify"]["message"]
-            await verify_channel.purge()
-            msg = await verify_channel.send(verify_message % (verify_emoji))
-            await msg.add_reaction(verify_emoji)
-            data["properties"][server]["events"]["on_reaction_add"]["verify"]["message_id"] = int(msg.id)
-            with open("properties.json", "w", encoding="UTF-8") as f:
-                f.write(json.dumps(data, indent=2))
-
+    guild = discord.utils.get(client.guilds, id=data["properties"]["general"]["guild_id"])
+    server = "general"
+    # elif int(guild.id) == data["properties"]["gaming"]["guild_id"]:
+    #     server = "gaming"
+    # verify_channel = discord.utils.get(guild.channels, id=data["properties"][server]["events"]["on_reaction_add"]["verify"]["channel"])
+    verify_channel = discord.utils.get(guild.channels, id=895385320848236574)
+    verify_emoji = data["properties"][server]["events"]["on_reaction_add"]["verify"]["emoji"]
+    verify_message = data["properties"][server]["events"]["on_reaction_add"]["verify"]["message"]
+    await verify_channel.purge()
+    msg = await verify_channel.send(verify_message % (verify_emoji))
+    await msg.add_reaction(verify_emoji)
+    data["properties"][server]["events"]["on_reaction_add"]["verify"]["message_id"] = int(msg.id)
+    with open("properties.json", "w", encoding="UTF-8") as f:
+        f.write(json.dumps(data, indent=2))
+    print("%sKahlifar Security: verify message sent" % PREFIX)
 
 
 # Commands  ----------------------------------------------------------------------------
@@ -169,6 +240,10 @@ async def send_deleted_msgs(amount, channel):
     msg = await channel.send("🗑Deleted `%s` messages" % amount)
     await asyncio.sleep(2)
     await msg.delete()
+
+
+async def rules(ctx):
+    await ctx.send("Rules comming soon")
 
 
 @client.command()
